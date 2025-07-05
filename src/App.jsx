a9ed1655e405db10f009d98a0c85f0b12096dc94
amsedrar1506 <amsedrar2202@gmail.com>
2025-07-05 18:39:38 +0100

    last UpDate

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,20 @@
-import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar.jsx";
-import HomePage from "./pages/HomePage.jsx";
-import AboutPage from "./pages/AboutPage.jsx";
-import MenuPage from "./pages/MenuPage.jsx";
-import BookingPage from "./pages/BookingPage.jsx";
-import OrderPage from "./pages/OrderPage.jsx";
-import LoginPage from "./pages/LoginPage.jsx";
-function App(){
-    return(
-        <BrowserRouter>
-                 <Navbar/>
-                 <Routes>
-                         <Route path="/" element={<HomePage/>}/>
-                         <Route path="/about" element={<AboutPage/>}/>
-                         <Route path="/menu" element={<MenuPage/>}/>
-                         <Route path="/booking" element={<BookingPage/>}/>
-                         <Route path="/order" element={<OrderPage/>}/>
-                         <Route path="/login" element={<LoginPage/>}/>
-                 </Routes>
-        </BrowserRouter>
-    );
+import React from 'react';
+import './App.css';
+import Nav from "./components/Nav"
+import Header from "./components/Header"
+import Main from "./components/Main"
+import Footer from "./components/Footer"
+
+function App() {
+  return (
+    <div>
+    <React.Fragment>
+      <Nav />
+      <Main />
+      <Footer />
+    </React.Fragment>
+    </div>
+  );
 }
+
 export default App;
--- src/pages/LoginPage.jsx
+++
@@ -1,4 +0,0 @@
-function LoginPage(){
-    return <h1>Login page</h1>;
-}
-export default LoginPage;
